75ab76d550cb7ec5d1e9e83d3598f56122a8cefb
boo <dicksonagbesi@gmail.com>
2025-07-07 11:30:30 +0000

    rebase error fixed

--- calc.sh
+++ calc.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 #################
 # this is a textchange 
 ## lets work this maths
 # 06/07/2025
 #branch name koko
-
+#####
 y=a+b
-<<<<<<< HEAD
+####
 z=8a/4a
 dy/dx=5x+2y-8
-=======
+###
 dydx= 5s+6j
->>>>>>> koko
 
+##
 g=9u*8y/4
+
+#
+b=7y+9-7o
+
